f0fdc019eac0a65cfc02bed8fc935ef9774ab806
rg2023 <rg2023>
2002-04-08 04:54:26 +0000

    1) Added IExecuteWorkflow Interface, only operation (currently)    ExecuteAlphaProtocol - alpha protocol = sequence of steps / workflow    to be executed by GateKeeper (may involve talking to external GKs to use    their services to complete workflow tasks)

---
+++ csharpsrc/DISCUS/IExecuteWorkflow.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace PSL.DISCUS.Interfaces
+{
+	/// <summary>
+	/// Summary description for IExecuteWorkflow.
+	/// </summary>
+	public interface IExecuteWorkflow
+	{
+		int ExecuteAlphaProtocol( string strXMLProtocol );
+	}
+}
