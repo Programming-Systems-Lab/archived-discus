4dda798c861a0914a1e71dacccf74612b98cf7d3
rg2023 <rg2023>
2002-05-05 22:44:45 +0000

    1) modified unit test

--- csharpsrc/DISCUSUnittest/Tester.cs
+++ csharpsrc/DISCUSUnittest/Tester.cs
@@ -11,7 +11,7 @@ using PSL.DISCUS.Impl.DynamicProxy;
 using PSL.DISCUS.Impl.DynamicProxy.Util;
 using System.Reflection;
 using System.Collections;
-//using DynamicPxy;
+//using DISCUSUnittest.localhost;
 
 
 namespace DISCUSUnittest
@@ -143,100 +143,20 @@ namespace DISCUSUnittest
 			{
 				//SetupServiceSpace();
 				//ResetServiceLocations();
-//				TestServiceMethods();
+				//TestServiceMethods();
 
-				
-
-
-
-
-			
-				//InternalRegistry ireg = new InternalRegistry();
-				//ireg.RegisterServiceMethod( "XMethodsQuery", "getServiceNamesByPublisher" );
-				//ireg.RegisterGateKeeper( "PSLGatekeeper1","", "http://localhost/PSLGatekeeper1/PSLGatekeeper1.asmx?WSDL", "http://localhost/PSLGatekeeper1/PSLGatekeeper1.asmx" );
-				//ireg.UpdateGateKeeperLocation( "PSLGatekeeper1", "http://localhost/PSLGatekeeper1/PSLGatekeeper1.asmx?WSDL" );
-
-				// Exec request with input paramters 
-				// XMethodsQuery - getServiceNamesByPublisher
-				// string strInput = "<?xml version=\"1.0\" encoding=\"utf-8\"?><ExecServiceMethodRequest><TreatyID>4</TreatyID><ServiceName>XMethodsQuery</ServiceName><MethodName>getServiceNamesByPublisher</MethodName><Parameter><![CDATA[<?xml version=\"1.0\"?><string>Interdata</string>]]></Parameter></ExecServiceMethodRequest>";
-				// Exec request no input parameters
-				// XMethodsQuery - getAllServiceSummaries
-				// string strInput = "<?xml version=\"1.0\" encoding=\"utf-8\"?><ExecServiceMethodRequest><TreatyID>4</TreatyID><ServiceName>XMethodsQuery</ServiceName><MethodName>getAllServiceSummaries</MethodName></ExecServiceMethodRequest>";
-				// QuoteBindingService
-				// string strInput = "<?xml version=\"1.0\" encoding=\"utf-8\"?><ExecServiceMethodRequest><TreatyID>4</TreatyID><ServiceName>QuoteBinding</ServiceName><MethodName>getPrice</MethodName><Parameter><![CDATA[<?xml version=\"1.0\"?><string>1861005458</string>]]></Parameter></ExecServiceMethodRequest>";
-				// ATMLocations service
-				// string strInput = "<?xml version=\"1.0\" encoding=\"utf-8\"?><ExecServiceMethodRequest><TreatyID>4</TreatyID><ServiceName>GeoCash</ServiceName><MethodName>GetATMLocations</MethodName><Parameter><![CDATA[<?xml version=\"1.0\"?><string>10025</string>]]></Parameter></ExecServiceMethodRequest>";
-
+				//string strAlpha = "<?xml version=\"1.0\" encoding=\"utf-8\"?><definitions name=\"DemoAlpha\" targetNamespace=\"http://psl.cs.columbia.edu\" xmlns:xlang=\"http://schemas.microsoft.com/bixtalk/xlang\" xmlns:xs=\"http://www.w3.org/2001/XMLSchema\"><!-- Simple demo using simple xlang constructs --><xlang:behavior><xlang:body><xlang:sequence><xlang:action activation=\"true\" gatekeeper=\"PSLGatekeeper1\" servicename=\"BNQuoteService\" operation=\"getPrice\"><parameter name=\"isbn\"><![CDATA[<?xml version=\"1.0\"?><string>1861005458</string>]]></parameter></xlang:action><xlang:action gatekeeper=\"PSLGatekeeper1\" servicename=\"GeoCash\" operation=\"GetATMLocations\"><parameter name=\"Zipcode\"><![CDATA[<?xml version=\"1.0\"?><string>10025</string>]]></parameter></xlang:action></xlang:sequence></xlang:body></xlang:behavior></definitions>";
 				//GateKeeper g = new GateKeeper();
-				//string strReturn = g.ExecuteServiceMethod( strInput );
-
-				//XmlDocument doc = new XmlDocument();
-				//doc.LoadXml( strReturn );
-
-				//XmlNode root = doc.DocumentElement;
-				
-				//string strData = root.InnerText;
-				
-				string strAlpha = "<?xml version=\"1.0\" encoding=\"utf-8\"?><definitions name=\"DemoAlpha\" targetNamespace=\"http://psl.cs.columbia.edu\" xmlns:xlang=\"http://schemas.microsoft.com/bixtalk/xlang\" xmlns:xs=\"http://www.w3.org/2001/XMLSchema\"><!-- Simple demo using simple xlang constructs --><xlang:behavior><xlang:body><xlang:sequence><xlang:action activation=\"true\" gatekeeper=\"PSLGatekeeper1\" servicename=\"BNQuoteService\" operation=\"getPrice\"><parameter name=\"isbn\"><![CDATA[<?xml version=\"1.0\"?><string>1861005458</string>]]></parameter></xlang:action><xlang:action gatekeeper=\"PSLGatekeeper1\" servicename=\"GeoCash\" operation=\"GetATMLocations\"><parameter name=\"Zipcode\"><![CDATA[<?xml version=\"1.0\"?><string>10025</string>]]></parameter></xlang:action><xlang:action gatekeeper=\"PSLGatekeeper1\" servicename=\"XMethodsQuery\" operation=\"getAllServiceSummaries\"></xlang:action></xlang:sequence></xlang:body></xlang:behavior></definitions>";
-				GateKeeper g = new GateKeeper();
-				string [] arrRes = g.ExecuteAlphaProtocol( strAlpha );
+				//string [] arrRes = g.ExecuteAlphaProtocol( strAlpha );
 
 				//string strTreaty = "<?xml version=\"1.0\"?><Treaty xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns=\"http://localhost/Discus/Schema/Treaty.xsd\"><ServiceInfo><ServiceMethod><Parameter>&lt;?xml version=\"1.0\"?&gt;&lt;string&gt;1861005458&lt;/string&gt;</Parameter><MethodName>getPrice</MethodName></ServiceMethod><ServiceName>BNQuoteService</ServiceName></ServiceInfo><ServiceInfo><ServiceMethod><Parameter>&lt;?xml version=\"1.0\"?&gt;&lt;string&gt;10025&lt;/string&gt;</Parameter><MethodName>GetATMLocations</MethodName></ServiceMethod><ServiceName>GeoCash</ServiceName></ServiceInfo><ServiceInfo><ServiceMethod><MethodName>getAllServiceSummaries</MethodName></ServiceMethod><ServiceName>XMethodsQuery</ServiceName></ServiceInfo><TreatyID>0</TreatyID><ClientServiceSpace>100</ClientServiceSpace><ProviderServiceSpace>PSLGatekeeper1</ProviderServiceSpace></Treaty>";
 				//g.EnlistServicesByName( strTreaty );
 				
-				/*ProxyGen pGen = new ProxyGen();
-				DynamicRequest req = new DynamicRequest();
-				req.baseURL = "http://localhost/PSLGatekeeper2/PSLGatekeeper2.asmx";
-				req.filenameSource = "PSLGatekeeper2";
-				req.serviceName = req.filenameSource;
-				req.wsdlFile = "http://localhost/PSLGatekeeper2/PSLGatekeeper2.asmx?WSDL";
-				req.proxyPath = "C:\\temp\\pxyCache";
+				//PSLGatekeeper2 g = new PSLGatekeeper2();
+				//string[] arrRes = g.ExecuteAlphaProtocol( strAlpha ); 
 
-				string strAssembly = pGen.GenerateAssembly( req );
-				*/
-				/*
-				PSLGatekeeper2 g = new PSLGatekeeper2();
-				string[] arrRes = g.ExecuteAlphaProtocol( strAlpha );
-				*/
-				
-				/*ProxyGen pGen = new ProxyGen();
-				DynamicRequest req = new DynamicRequest();
-				req.baseURL = "http://church.psl.cs.columbia.edu:8080/security/jaxrpc/SecurityManagerService";
-				req.filenameSource = "SecurityManagerService";
-				req.serviceName = req.filenameSource;
-				req.wsdlFile = "http://church.psl.cs.columbia.edu:8080/security/SecurityServices.wsdl";
-				req.proxyPath = "C:\\temp\\pxyCache";
-				
-				string strAssembly = pGen.GenerateAssembly( req );*/
 
-				//InternalRegistry ireg = new InternalRegistry();
-				//int nServiceID = ireg.RegisterService( "SecurityManagerService", "", "http://church.psl.cs.columbia.edu:8080/security/SecurityServices.wsdl", "http://church.psl.cs.columbia.edu:8080/security/jaxrpc/SecurityManagerService" ); 
-				//if( nServiceID != -1 )
-				//	ireg.RegisterServiceMethod( "SecurityManagerService", "verifyTreaty" );
-				//*/
-				//ireg.UpdateServiceLocation( "SecurityManagerService", "http://church.psl.cs.columbia.edu:8080/security/SecurityServices.wsdl" );
-				
-				//string strTreaty = "<?xml version=\"1.0\" ?><Treaty xmlns=\"http://localhost/Discus/Schema/Treaty.xsd\"><TreatyID>1000</TreatyID><ClientServiceSpace>myservicespace</ClientServiceSpace><ProviderServiceSpace>providerss</ProviderServiceSpace><ServiceInfo><ServiceName>service</ServiceName><ServiceMethod><MethodName>method</MethodName><Parameter>foo</Parameter><Parameter>bar</Parameter><NumInvokations>1</NumInvokations><Authorized>true</Authorized></ServiceMethod></ServiceInfo></Treaty>";
-				/*string strInput = "<?xml version=\"1.0\" encoding=\"utf-8\"?><ExecServiceMethodRequest><TreatyID>1000</TreatyID><ServiceName>SecurityManagerService</ServiceName><MethodName>verifyTreaty</MethodName><Parameter><![CDATA[<?xml version=\"1.0\"?><string><?xml version=\"1.0\"?><Treaty xmlns=\"http://localhost/Discus/Schema/Treaty.xsd\"><TreatyID>1000</TreatyID><ClientServiceSpace>myservicespace</ClientServiceSpace><ProviderServiceSpace>providerss</ProviderServiceSpace><ServiceInfo><ServiceName>service</ServiceName><ServiceMethod><MethodName>method</MethodName><Parameter>foo</Parameter><Parameter>bar</Parameter><NumInvokations>1</NumInvokations><Authorized>true</Authorized></ServiceMethod></ServiceInfo></Treaty></string>]]></Parameter></ExecServiceMethodRequest>";
-				GateKeeper g = new GateKeeper();
-				string strRes = g.ExecuteServiceMethod( strInput );
 
-				XmlDocument doc = new XmlDocument();
-				doc.LoadXml( strRes );
-
-				string strT = doc.InnerText;
-				string strX = doc.InnerXml;
-				
-				//XmlSerializer xser = new XmlSerializer( System.String );
-				//XmlTextReader xt = new XmlTextReader( strRes, XmlNodeType.Document );
-				//string strSMgrResponse = xt.ReadInnerXml();
-				
-				//XmlDocument doc = new XmlDocument();
-				//doc.LoadXml( strRes );*/
-
-				 
-
-				
 				int x = 0;
 				x++;
 			}
