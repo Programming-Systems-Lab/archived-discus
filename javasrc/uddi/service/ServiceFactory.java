f68b832abf5c224c4a9b03045ed7bdc16f7938cc
mp2079 <mp2079>
2002-08-25 01:47:22 +0000

    update package names to discus package

--- javasrc/uddi/service/ServiceFactory.java
+++ javasrc/uddi/service/ServiceFactory.java
@@ -21,11 +21,11 @@ import java.util.Hashtable;
 
 /**
  * Implementation of Factory pattern used to create an implementation of
- * the org.juddi.auth.Authenticator interface.
+ * the psl.discus.javasrc.uddi.auth.Authenticator interface.
  *
  * The name of the Authenticator implementation to create is passed to the
  * getAuthenticator method.  If a null value is passed then the default
- * Authenticator implementation "org.juddi.auth.SimpleAuthenticator" is
+ * Authenticator implementation "psl.discus.javasrc.uddi.auth.SimpleAuthenticator" is
  * created.
  *
  * @author  Steve Viens
@@ -48,62 +48,62 @@ public abstract class ServiceFactory
     // the value passed in. - Steve
 
     // use to obtain the service class via the UDDI element name (read IMPORTANT above regarding case)
-    serviceTable.put("add_publisherassertions",                       "org.juddi.service.AddPublisherAssertionsService");
-    serviceTable.put("delete_binding",                                "org.juddi.service.DeleteBindingService");
-    serviceTable.put("delete_business",                               "org.juddi.service.DeleteBusinessService");
-    serviceTable.put("delete_publisherassertions",                    "org.juddi.service.DeletePublisherAssertionsService");
-    serviceTable.put("delete_service",                                "org.juddi.service.DeleteServiceService");
-    serviceTable.put("delete_tmodel",                                 "org.juddi.service.DeleteTModelService");
-    serviceTable.put("discard_authtoken",                             "org.juddi.service.DiscardAuthTokenService");
-    serviceTable.put("find_binding",                                  "org.juddi.service.FindBindingService");
-    serviceTable.put("find_business",                                 "org.juddi.service.FindBusinessService");
-    serviceTable.put("find_relatedbusinesses",                        "org.juddi.service.FindRelatedBusinessesService");
-    serviceTable.put("find_service",                                  "org.juddi.service.FindServiceService");
-    serviceTable.put("find_tmodel",                                   "org.juddi.service.FindTModelService");
-    serviceTable.put("get_assertionstatusreport",                     "org.juddi.service.GetAssertionStatusReportService");
-    serviceTable.put("get_authtoken",                                 "org.juddi.service.GetAuthTokenService");
-    serviceTable.put("get_bindingdetail",                             "org.juddi.service.GetBindingDetailService");
-    serviceTable.put("get_businessdetailext",                         "org.juddi.service.GetBusinessDetailExtService");
-    serviceTable.put("get_businessdetail",                            "org.juddi.service.GetBusinessDetailService");
-    serviceTable.put("get_publisherassertions",                       "org.juddi.service.GetPublisherAssertionsService");
-    serviceTable.put("get_registeredinfo",                            "org.juddi.service.GetRegisteredInfoService");
-    serviceTable.put("get_servicedetail",                             "org.juddi.service.GetServiceDetailService");
-    serviceTable.put("get_tmodeldetail",                              "org.juddi.service.GetTModelDetailService");
-    serviceTable.put("save_binding",                                  "org.juddi.service.SaveBindingService");
-    serviceTable.put("save_business",                                 "org.juddi.service.SaveBusinessService");
-    serviceTable.put("save_service",                                  "org.juddi.service.SaveServiceService");
-    serviceTable.put("save_tmodel",                                   "org.juddi.service.SaveTModelService");
-    serviceTable.put("set_publisherassertions",                       "org.juddi.service.SetPublisherAssertionsService");
-    serviceTable.put("validate_values",                               "org.juddi.service.ValidateValuesService");
+    serviceTable.put("add_publisherassertions",                       "psl.discus.javasrc.uddi.service.AddPublisherAssertionsService");
+    serviceTable.put("delete_binding",                                "psl.discus.javasrc.uddi.service.DeleteBindingService");
+    serviceTable.put("delete_business",                               "psl.discus.javasrc.uddi.service.DeleteBusinessService");
+    serviceTable.put("delete_publisherassertions",                    "psl.discus.javasrc.uddi.service.DeletePublisherAssertionsService");
+    serviceTable.put("delete_service",                                "psl.discus.javasrc.uddi.service.DeleteServiceService");
+    serviceTable.put("delete_tmodel",                                 "psl.discus.javasrc.uddi.service.DeleteTModelService");
+    serviceTable.put("discard_authtoken",                             "psl.discus.javasrc.uddi.service.DiscardAuthTokenService");
+    serviceTable.put("find_binding",                                  "psl.discus.javasrc.uddi.service.FindBindingService");
+    serviceTable.put("find_business",                                 "psl.discus.javasrc.uddi.service.FindBusinessService");
+    serviceTable.put("find_relatedbusinesses",                        "psl.discus.javasrc.uddi.service.FindRelatedBusinessesService");
+    serviceTable.put("find_service",                                  "psl.discus.javasrc.uddi.service.FindServiceService");
+    serviceTable.put("find_tmodel",                                   "psl.discus.javasrc.uddi.service.FindTModelService");
+    serviceTable.put("get_assertionstatusreport",                     "psl.discus.javasrc.uddi.service.GetAssertionStatusReportService");
+    serviceTable.put("get_authtoken",                                 "psl.discus.javasrc.uddi.service.GetAuthTokenService");
+    serviceTable.put("get_bindingdetail",                             "psl.discus.javasrc.uddi.service.GetBindingDetailService");
+    serviceTable.put("get_businessdetailext",                         "psl.discus.javasrc.uddi.service.GetBusinessDetailExtService");
+    serviceTable.put("get_businessdetail",                            "psl.discus.javasrc.uddi.service.GetBusinessDetailService");
+    serviceTable.put("get_publisherassertions",                       "psl.discus.javasrc.uddi.service.GetPublisherAssertionsService");
+    serviceTable.put("get_registeredinfo",                            "psl.discus.javasrc.uddi.service.GetRegisteredInfoService");
+    serviceTable.put("get_servicedetail",                             "psl.discus.javasrc.uddi.service.GetServiceDetailService");
+    serviceTable.put("get_tmodeldetail",                              "psl.discus.javasrc.uddi.service.GetTModelDetailService");
+    serviceTable.put("save_binding",                                  "psl.discus.javasrc.uddi.service.SaveBindingService");
+    serviceTable.put("save_business",                                 "psl.discus.javasrc.uddi.service.SaveBusinessService");
+    serviceTable.put("save_service",                                  "psl.discus.javasrc.uddi.service.SaveServiceService");
+    serviceTable.put("save_tmodel",                                   "psl.discus.javasrc.uddi.service.SaveTModelService");
+    serviceTable.put("set_publisherassertions",                       "psl.discus.javasrc.uddi.service.SetPublisherAssertionsService");
+    serviceTable.put("validate_values",                               "psl.discus.javasrc.uddi.service.ValidateValuesService");
 
     // use to obtain the service class via the UDDI request class name (read IMPORTANT above regarding case)
-    serviceTable.put("org.uddi4j.request.addpublisherassertions",     "org.juddi.service.AddPublisherAssertionsService");
-    serviceTable.put("org.uddi4j.request.deletebinding",              "org.juddi.service.DeleteBindingService");
-    serviceTable.put("org.uddi4j.request.deletebusiness",             "org.juddi.service.DeleteBusinessService");
-    serviceTable.put("org.uddi4j.request.deletepublisherassertions",  "org.juddi.service.DeletePublisherAssertionsService");
-    serviceTable.put("org.uddi4j.request.deleteservice",              "org.juddi.service.DeleteServiceService");
-    serviceTable.put("org.uddi4j.request.deletetmodel",               "org.juddi.service.DeleteTModelService");
-    serviceTable.put("org.uddi4j.request.discardauthtoken",           "org.juddi.service.DiscardAuthTokenService");
-    serviceTable.put("org.uddi4j.request.findbinding",                "org.juddi.service.FindBindingService");
-    serviceTable.put("org.uddi4j.request.findbusiness",               "org.juddi.service.FindBusinessService");
-    serviceTable.put("org.uddi4j.request.findrelatedbusinesses",      "org.juddi.service.FindRelatedBusinessesService");
-    serviceTable.put("org.uddi4j.request.findservice",                "org.juddi.service.FindServiceService");
-    serviceTable.put("org.uddi4j.request.findtmodel",                 "org.juddi.service.FindTModelService");
-    serviceTable.put("org.uddi4j.request.getassertionstatusreport",   "org.juddi.service.GetAssertionStatusReportService");
-    serviceTable.put("org.uddi4j.request.getauthtoken",               "org.juddi.service.GetAuthTokenService");
-    serviceTable.put("org.uddi4j.request.getbindingdetail",           "org.juddi.service.GetBindingDetailService");
-    serviceTable.put("org.uddi4j.request.getbusinessdetailext",       "org.juddi.service.GetBusinessDetailExtService");
-    serviceTable.put("org.uddi4j.request.getbusinessdetail",          "org.juddi.service.GetBusinessDetailService");
-    serviceTable.put("org.uddi4j.request.getpublisherassertions",     "org.juddi.service.GetPublisherAssertionsService");
-    serviceTable.put("org.uddi4j.request.getregisteredInfo",          "org.juddi.service.GetRegisteredInfoService");
-    serviceTable.put("org.uddi4j.request.getservicedetail",           "org.juddi.service.GetServiceDetailService");
-    serviceTable.put("org.uddi4j.request.gettmodeldetail",            "org.juddi.service.GetTModelDetailService");
-    serviceTable.put("org.uddi4j.request.savebinding",                "org.juddi.service.SaveBindingService");
-    serviceTable.put("org.uddi4j.request.savebusiness",               "org.juddi.service.SaveBusinessService");
-    serviceTable.put("org.uddi4j.request.saveservice",                "org.juddi.service.SaveServiceService");
-    serviceTable.put("org.uddi4j.request.savetmodel",                 "org.juddi.service.SaveTModelService");
-    serviceTable.put("org.uddi4j.request.setpublisherassertions",     "org.juddi.service.SetPublisherAssertionsService");
-    serviceTable.put("org.uddi4j.request.validatevalues",             "org.juddi.service.ValidateValuesService");
+    serviceTable.put("org.uddi4j.request.addpublisherassertions",     "psl.discus.javasrc.uddi.service.AddPublisherAssertionsService");
+    serviceTable.put("org.uddi4j.request.deletebinding",              "psl.discus.javasrc.uddi.service.DeleteBindingService");
+    serviceTable.put("org.uddi4j.request.deletebusiness",             "psl.discus.javasrc.uddi.service.DeleteBusinessService");
+    serviceTable.put("org.uddi4j.request.deletepublisherassertions",  "psl.discus.javasrc.uddi.service.DeletePublisherAssertionsService");
+    serviceTable.put("org.uddi4j.request.deleteservice",              "psl.discus.javasrc.uddi.service.DeleteServiceService");
+    serviceTable.put("org.uddi4j.request.deletetmodel",               "psl.discus.javasrc.uddi.service.DeleteTModelService");
+    serviceTable.put("org.uddi4j.request.discardauthtoken",           "psl.discus.javasrc.uddi.service.DiscardAuthTokenService");
+    serviceTable.put("org.uddi4j.request.findbinding",                "psl.discus.javasrc.uddi.service.FindBindingService");
+    serviceTable.put("org.uddi4j.request.findbusiness",               "psl.discus.javasrc.uddi.service.FindBusinessService");
+    serviceTable.put("org.uddi4j.request.findrelatedbusinesses",      "psl.discus.javasrc.uddi.service.FindRelatedBusinessesService");
+    serviceTable.put("org.uddi4j.request.findservice",                "psl.discus.javasrc.uddi.service.FindServiceService");
+    serviceTable.put("org.uddi4j.request.findtmodel",                 "psl.discus.javasrc.uddi.service.FindTModelService");
+    serviceTable.put("org.uddi4j.request.getassertionstatusreport",   "psl.discus.javasrc.uddi.service.GetAssertionStatusReportService");
+    serviceTable.put("org.uddi4j.request.getauthtoken",               "psl.discus.javasrc.uddi.service.GetAuthTokenService");
+    serviceTable.put("org.uddi4j.request.getbindingdetail",           "psl.discus.javasrc.uddi.service.GetBindingDetailService");
+    serviceTable.put("org.uddi4j.request.getbusinessdetailext",       "psl.discus.javasrc.uddi.service.GetBusinessDetailExtService");
+    serviceTable.put("org.uddi4j.request.getbusinessdetail",          "psl.discus.javasrc.uddi.service.GetBusinessDetailService");
+    serviceTable.put("org.uddi4j.request.getpublisherassertions",     "psl.discus.javasrc.uddi.service.GetPublisherAssertionsService");
+    serviceTable.put("org.uddi4j.request.getregisteredInfo",          "psl.discus.javasrc.uddi.service.GetRegisteredInfoService");
+    serviceTable.put("org.uddi4j.request.getservicedetail",           "psl.discus.javasrc.uddi.service.GetServiceDetailService");
+    serviceTable.put("org.uddi4j.request.gettmodeldetail",            "psl.discus.javasrc.uddi.service.GetTModelDetailService");
+    serviceTable.put("org.uddi4j.request.savebinding",                "psl.discus.javasrc.uddi.service.SaveBindingService");
+    serviceTable.put("org.uddi4j.request.savebusiness",               "psl.discus.javasrc.uddi.service.SaveBusinessService");
+    serviceTable.put("org.uddi4j.request.saveservice",                "psl.discus.javasrc.uddi.service.SaveServiceService");
+    serviceTable.put("org.uddi4j.request.savetmodel",                 "psl.discus.javasrc.uddi.service.SaveTModelService");
+    serviceTable.put("org.uddi4j.request.setpublisherassertions",     "psl.discus.javasrc.uddi.service.SetPublisherAssertionsService");
+    serviceTable.put("org.uddi4j.request.validatevalues",             "psl.discus.javasrc.uddi.service.ValidateValuesService");
   }
 
   /**
@@ -125,7 +125,7 @@ public abstract class ServiceFactory
       throw new JUDDIException();
     }
 
-    // look up the name of the org.juddi.UDDIService subclass in the
+    // look up the name of the psl.discus.javasrc.uddi.UDDIService subclass in the
     // serviceTable hashtable. First we conver the serviceName passed in to
     // lower case so we have a better chance of finding a match - in case the
     // UDDI client used to make the call doesn't pass the request in the
@@ -134,7 +134,7 @@ public abstract class ServiceFactory
 
     if ((serviceClassName == null) || (serviceClassName.length() == 0))
     {
-      log.error("An implementation of the org.juddi.service.UDDIService " +
+      log.error("An implementation of the psl.discus.javasrc.uddi.service.UDDIService " +
         "class was not found for the serviceName: " + serviceName);
       throw new JUDDIException();
     }
@@ -156,7 +156,7 @@ public abstract class ServiceFactory
     }
     catch (ClassNotFoundException ex)
     {
-      System.out.println("The subclass of org.juddi.service.UDDIService " +
+      System.out.println("The subclass of psl.discus.javasrc.uddi.service.UDDIService " +
   "class specified was not found in classpath: " + serviceClassName +
   " not found.");
       throw new JUDDIException();
@@ -164,7 +164,7 @@ public abstract class ServiceFactory
     catch (InstantiationException ex)
     {
       System.out.println("Exception while instantiating the specified " +
-        "subclass of org.juddi.service.UDDIService: " + serviceClassName);
+        "subclass of psl.discus.javasrc.uddi.service.UDDIService: " + serviceClassName);
       throw new JUDDIException();
     }
     catch (IllegalAccessException ex)
@@ -173,7 +173,7 @@ public abstract class ServiceFactory
     }
 
     throw new RuntimeException("Could not create instance of the " +
-      "org.juddi.UDDIService subclass specified: " + serviceClassName);
+      "psl.discus.javasrc.uddi.UDDIService subclass specified: " + serviceClassName);
   }
 
 
